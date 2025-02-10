30cbf1379d520b89f9c16a777657a4111ba3abcb
Ángel Ilai Alcañiz García <softdevilan@proton.me>
2025-02-10 22:18:26 +0100

    sidebar improvements

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../../../services/firebase.service';
 import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
+import { ClientListComponent } from '../clients/client-list/client-list.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, ClientListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
